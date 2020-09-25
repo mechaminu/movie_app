ef7cb6fb8010eaae7a5b13ae4992d022ad11e477
Minwoo <lmw5201@gmail.com>
2020-09-26 02:12:49 +0900

    Component Lifecycle

--- src/ClassApp.js
+++ src/ClassApp.js
@@ -4,22 +4,52 @@ import React from "react";
 
 class ClassApp extends React.Component {
 
+    constructor(p) {
+        super(p);
+        console.log("Hello, world!");
+    }
+
     // 바뀌는 데이터를 담는 그릇, State
     state = {
         count: 0
     };
-    
+
     // React re-render page for every setState execution
     plus = () => {
-        this.setState(c => ({count: c.count + 1}));
+        this.setState(c => ({ count: c.count + 1 }));
     };
 
     minus = () => {
-        this.setState(c => ({count: c.count - 1}));
+        this.setState(c => ({ count: c.count - 1 }));
     };
+
+    /** Component Lifecycle Call stack
+     *  - mounting
+     *      constructor
+     *      render
+     *      componentDidMount
+     *  - updating
+     *      render
+     *      componentDidUpdate
+     *  - unmounting
+     *      componentWillUnmount
+     */
+
+    componentDidMount() {
+        console.log("rendered");
+    }
+
+    componentDidUpdate() {
+        console.log("updated");
+    }
+
+    componentWillUnmount() {
+        console.log("Goodbye, cruel world...!")
+    }
 
     // React는 자동적으로 Class component의 render 메소드를 실행한다!
     render() {
+        console.log("rendering");
         return <div>
             <h1>I'm a class component</h1>
             <div>
